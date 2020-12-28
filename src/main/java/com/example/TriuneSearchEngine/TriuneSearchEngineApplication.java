6ae2663eb94a429c4c74e998b3a57c87675c3bac
hillary <hillary@zipcodes-MBP.home>
2020-12-27 20:14:12 -0500

    initial commit

---
+++ src/main/java/com/example/TriuneSearchEngine/TriuneSearchEngineApplication.java
@@ -0,0 +1,13 @@
+package com.example.TriuneSearchEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriuneSearchEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriuneSearchEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TriuneSearchEngine/TriuneSearchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TriuneSearchEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriuneSearchEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
